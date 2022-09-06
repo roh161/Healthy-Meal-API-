64b98f3f5752c4ebadc71765319f3aa62203d8f0
roh161 <rohit.t@webkorps.com>
2022-09-06 19:32:52 +0530

    Improved user controller

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,7 +3,7 @@ class Users::UsersController < ApplicationController
 
     def index
         @users = User.all
-        render json: { users: @users }, status: 200
+        render json: { All_users: @users }, status: 200
     end
 
     def show
